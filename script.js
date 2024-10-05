450a0d233c7d3a4518b2fed44d146bd6376a4e87
wolforce <miguelfeotorres@gmail.com>
2024-10-05 21:35:47 +0100

    v0.2

--- script.js
+++ script.js
@@ -1,43 +1,75 @@
-function start() {
 
+function start(element) {
+
+    document.getElementById('initial').classList.add('hidden');
+    document.getElementById('percentages').classList.remove('hidden');
+
+    const poll = {};
     const canvas = document.getElementById('canvas');
+    canvas.classList.remove("hidden");
     const ctx = canvas.getContext('2d');
-    const ws = new WebSocket('wss://elementalpixelserver.onrender.com');
+    const ws = new WebSocket('ws://localhost');
+    // const ws = new WebSocket('wss://elementalpixelserver.onrender.com');
 
     let lastDirection = "left";
     let lastMessage;
 
-    window.addEventListener('keydown', ({ key }) => {
-        if (key === 'ArrowUp') {
-            send({ type: "move", dx: 0, dy: -1 });
+    window.addEventListener('keydown', ({ key }) => poll[key] = true);
+    window.addEventListener('keyup', ({ key }) => poll[key] = false);
+
+    setInterval(() => {
+
+        let keyPressed = false, dx = 0, dy = 0;
+
+        if (poll['ArrowUp']) {
+            dy = -1;
+            keyPressed = true;
+            // send({ type: "move", dx: 0, dy: -1 });
             lastDirection = "up";
-        } else if (key === 'ArrowDown') {
-            send({ type: "move", dx: 0, dy: 1 });
-            lastDirection = "down";
-        } else if (key === 'ArrowLeft') {
-            send({ type: "move", dx: -1, dy: 0 });
-            lastDirection = "left";
-        } else if (key === 'ArrowRight') {
-            send({ type: "move", dx: 1, dy: 0 });
-            lastDirection = "right";
-        } else if (key === 'q') {
-            send({ type: "action1", dir: lastDirection });
-        } else if (key === 'w') {
-            send({ type: "action2", dir: lastDirection });
-        } else if (key === 'e') {
-            send({ type: "action3", dir: lastDirection });
-        } else if (key === 'r') {
-            send({ type: "action4", dir: lastDirection });
-        } else {
-            // console.log(key);
         }
-    });
+        if (poll['ArrowDown']) {
+            dy = 1;
+            keyPressed = true;
+            // send({ type: "move", dx: 0, dy: 1 });
+            lastDirection = "down";
+        }
+        if (poll['ArrowLeft']) {
+            dx = -1;
+            keyPressed = true;
+            // send({ type: "move", dx: -1, dy: 0 });
+            lastDirection = "left";
+        }
+        if (poll['ArrowRight']) {
+            dx = 1;
+            keyPressed = true;
+            // send({ type: "move", dx: 1, dy: 0 });
+            lastDirection = "right";
+        }
+        if (keyPressed) {
+            send({ type: "move", dx, dy });
+        }
+
+        if (poll['q']) {
+            send({ type: "action1", dir: lastDirection });
+        }
+        if (poll['w']) {
+            send({ type: "action2", dir: lastDirection });
+        }
+        if (poll['e']) {
+            send({ type: "action3", dir: lastDirection });
+        }
+        if (poll['r']) {
+            send({ type: "action4", dir: lastDirection });
+        }
+
+    }, 1000 / 20);
 
     function send(msg) {
         ws.send(JSON.stringify(msg));
     }
+
     ws.onopen = () => {
-        ws.send(`{"type":"init","element":"water"}`)
+        ws.send(`{"type":"init","element":"${element}"}`)
     }
 
     ws.onmessage = (unparsedMessage) => {
@@ -60,7 +92,12 @@ function start() {
 
             renderPos(0, 0, "#FFFFFF55")
 
-            document.getElementById("coord").innerHTML = message.player.x + "," + message.player.y
+            document.getElementById("coord").innerHTML = message.player.x + "," + message.player.y;
+            document.getElementById("waterPercent").innerHTML = message.percentages[0] + "%";
+            document.getElementById("earthPercent").innerHTML = message.percentages[1] + "%";
+            document.getElementById("firePercent").innerHTML = message.percentages[2] + "%";
+            document.getElementById("airPercent").innerHTML = message.percentages[3] + "%";
+
 
         } catch (e) {
             console.log(e);
@@ -81,11 +118,13 @@ function start() {
     function getColor(element) {
         switch (element) {
             case "water": return "#0044FF";
-            case "w": return "#0033AA";
-            case "a": return "#AA0000";
-            case "s": return "#00AA00";
-            case "d": return "#AAAAAA";
-            case "0": return "#FF0000";
+            case "earth": return "#188b48";
+            case "fire": return "#c42408";
+            case "air": return "#fffeaf";
+            case "w": return "#0044FF";
+            case "e": return "#188b48";
+            case "f": return "#c42408";
+            case "a": return "#fffeaf";
         }
         // let i = element.charCodeAt(0) - 12;
         // return "#" + i + i + i;
@@ -105,5 +144,3 @@ function start() {
     }
 
 }
-
-start();
